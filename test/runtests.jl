75afe1535a7274f4c9df013dd63f1101dcf0895a
Ed Scheinerman <ers@jhu.edu>
2018-08-18 10:33:05 -0400

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,8 @@
 using Test
 using SimpleGraphs
 
+Pkg.clone("https://github.com/scheinerman/SimpleRandom.jl.git")
+Pkg.clone("https://github.com/scheinerman/SimplePartitions.jl.git")
 # This is woefully inadequate. Just a placeholder for now.
 
 G = Petersen()
