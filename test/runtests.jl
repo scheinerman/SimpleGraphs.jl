1a82b5b45bffbc5753021679791fbb305703853b
Ed Scheinerman <ers@jhu.edu>
2018-08-18 15:42:48 -0400

    Really bad runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,10 @@
 using Test
 using SimpleGraphs
+using LinearAlgebra
+using SparseArrays
 
-Pkg.clone("https://github.com/scheinerman/SimpleRandom.jl.git")
-Pkg.clone("https://github.com/scheinerman/SimplePartitions.jl.git")
+# Pkg.clone("https://github.com/scheinerman/SimpleRandom.jl.git")
+# Pkg.clone("https://github.com/scheinerman/SimplePartitions.jl.git")
 # This is woefully inadequate. Just a placeholder for now.
 
 G = Petersen()
