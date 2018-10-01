33a21e1d52cdeb73783d8f56f4a1e2b6dc858b06
rbao2 <rbao2@jhu.edu>
2018-10-01 00:47:49 -0400

    save changes to euler path

--- src/d_simple_core.jl
+++ src/d_simple_core.jl
@@ -476,6 +476,7 @@ function euler_work!(G::SimpleDigraph{T}, u::T) where {T}
         if length(Nu) == 1
             v = Nu[1]
             append!(trail,u)
+            delete!(G,u,v)
             delete!(G,u)
             u = v
         else
