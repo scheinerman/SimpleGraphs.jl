70d4293f644f5750db0d2d090e75719399d3b0d9
Ed Scheinerman <ers@jhu.edu>
2022-07-16 15:31:06 -0400

    Transition names

--- src/abbreviations.jl
+++ src/abbreviations.jl
@@ -2,3 +2,12 @@ const UG = SimpleGraph
 const DG = SimpleDigraph
 const HG = SimpleHypergraph
 export UG, DG, HG
+
+
+# These names will not be necessary in versions 0.8.1 and on
+
+const UndirectedGraph = SimpleGraph
+const DirectedGraph = SimpleDigraph
+const Hypergraph = SimpleHypergraph
+
+export UndirectedGraph, DirectedGraph, Hypergraph
