266c8cc4f6f549daf6a071dfa94c3b6a5a1e9a3a
Ed Scheinerman <ers@jhu.edu>
2022-07-18 11:09:14 -0400

    Update graph_converter.jl

--- extras/graph_converter.jl
+++ extras/graph_converter.jl
@@ -3,23 +3,7 @@ using SimpleGraphs, Graphs
 import SimpleGraphs: UndirectedGraph, UG 
 import Graphs: SimpleGraph
 
-"""
-    graph_convert 
-Both `Graphs` and `SimpleGraphs` define a graph type named `SimpleGraph`.
-This function is used to convert between the two.
 
-If `g` is a `Graphs.SimpleGraph` then `graph_convert(g)` returns a 
-`SimpleGraphs.SimpleGraph` with exactly the same vertices and edges as `g`.
-
-Conversion in the other direction is more complicated because the `SimpleGraphs` 
-module allows  arbitrary sets of vertices, whereas `Graphs` require graphs to have 
-vertex sets of the form `{1,2,...,n}`. 
-
-When `graph_convert` is applied to a graph `G`
-of type `SimpleGraphs.SimpleGraph` it first makes a copy of `G` with vertices 
-relabeled to be integers from `1` to `n`, and then uses that copy to make a 
-`Graphs.SimpleGraph`. See `SimpleGraphs.relabel`.
-"""
 function UndirectedGraph(g::SimpleGraph{T})::UG{T} where {T}
     G = UG{T}()
 
